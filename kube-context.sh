5db0bf6cfb2bffc41976e9b3a7e0b7d76c08dcbf
Zachariah Dzielinski <46588571+zdzielinski@users.noreply.github.com>
2020-10-16 02:11:09 -0400

    Update kube-context.sh

--- kube-context.sh
+++ kube-context.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # default kube config file, check for provided directory
-DEFAULT_KUBECONFIG=${1-"$HOME/.kube/config"}
+DEFAULT_KUBECONFIG="$HOME/.kube/config"
 
 # if it exists, add it into the KUBECONFIG variable
 # otherwise, clear it for further use
@@ -10,7 +10,7 @@ if test -f $DEFAULT_KUBECONFIG; then
 else export KUBECONFIG=""; fi
 
 # set and create the custom kube contexts directory
-CONTEXTS="$HOME/.kube/contexts"; mkdir -p $CONTEXTS
+CONTEXTS="${1-"$HOME/.kube/contexts"}"; mkdir -p $CONTEXTS
 
 # find all .yml and .yaml files in the custom contexts directory
 # and incorporate them into the KUBECONFIG variable for easier context
