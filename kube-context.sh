8d56522653ce44353824a693f769eca2a63a3a92
Zachariah Dzielinski <46588571+zdzielinski@users.noreply.github.com>
2020-10-16 01:07:07 -0400

    Update kube-context.sh

--- kube-context.sh
+++ kube-context.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 
-# helper function for cli usage
-function kube-context {
-  # default kube config file
-  DEFAULT_KUBECONFIG="$HOME/.kube/config"
+# default kube config file, check for provided directory
+DEFAULT_KUBECONFIG=${1-"$HOME/.kube/config"}
 
-  # if it exists, add it into the KUBECONFIG variable
-  # otherwise, clear it for further use
-  if test -f $DEFAULT_KUBECONFIG; then
-    export KUBECONFIG="$DEFAULT_KUBECONFIG:"
-  else; export KUBECONFIG=""; fi
+echo $DEFAULT_KUBECONFIG
 
-  # set and create the custom kube contexts directory
-  CONTEXTS="$HOME/.kube/contexts"; mkdir -p $CONTEXTS
+# if it exists, add it into the KUBECONFIG variable
+# otherwise, clear it for further use
+if test -f $DEFAULT_KUBECONFIG; then
+  export KUBECONFIG="$DEFAULT_KUBECONFIG:"
+else; export KUBECONFIG=""; fi
 
-  # find all .yml and .yaml files in the custom contexts directory
-  # and incorporate them into the KUBECONFIG variable for easier context
-  # switching
-  find $CONTEXTS -type f \( -name "*.yml" -o -name "*.yaml" \) \
-    -print0 | while IFS= read -r -d '' context; do
-      export KUBECONFIG="${KUBECONFIG}${context}:"
-    done
+# set and create the custom kube contexts directory
+CONTEXTS="$HOME/.kube/contexts"; mkdir -p $CONTEXTS
 
-  # make sure the KUBECONFIG variable looks clean
-  export KUBECONFIG="${KUBECONFIG: :-1}"
-}
+# find all .yml and .yaml files in the custom contexts directory
+# and incorporate them into the KUBECONFIG variable for easier context
+# switching
+find $CONTEXTS -type f \( -name "*.yml" -o -name "*.yaml" \) \
+  -print0 | while IFS= read -r -d '' context; do
+    export KUBECONFIG="${KUBECONFIG}${context}:"
+  done
 
-# call the helper function once
-kube-context
+# make sure the KUBECONFIG variable looks clean
+export KUBECONFIG="${KUBECONFIG: :-1}"
